4feff5a0316b23e86b47df5b97b2034c202f833e
cynthiarivera15 <cynthia.rivera15@upr.edu>
2022-02-12 22:32:43 -0400

    More updates

--- js/contact_me.js
+++ js/contact_me.js
@@ -35,7 +35,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Your message has been sent. You will receive a respond within three to five business days</strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
